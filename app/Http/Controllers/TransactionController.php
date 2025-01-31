288c2cc4e299f29107ef537305fe6ad5712e3bdc
fendnugraha <fendnugraha92@gmail.com>
2025-01-31 12:51:49 +0700

    sales masih error

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Journal;
+use App\Models\Product;
 use App\Models\Transaction;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\AccountResource;
 
@@ -32,7 +36,72 @@ class TransactionController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'cart' => 'required|array',
+            'transaction_type' => 'required|string',
+        ]);
+
+
+        // $modal = $this->modal * $this->quantity;
+
+        $invoice = Journal::invoice_journal();
+
+        DB::beginTransaction();
+        try {
+            foreach ($request->cart as $item) {
+                $journal = new Journal();
+                $price = $item['price'] * $item['quantity'];
+                $cost = Product::find($item['id'])->cost;
+                $modal = $cost * $item['quantity'];
+
+                $description = "Penjualan Accessories";
+                $fee = $price - $modal;
+
+                // $journal->create([
+                //     'invoice' => $invoice,  // Menggunakan metode statis untuk invoice
+                //     'date_issued' => now(),
+                //     'debt_code' => 10,
+                //     'cred_code' => 10,
+                //     'amount' => $price,
+                //     'fee_amount' => $fee,
+                //     'trx_type' => 'Accessories',
+                //     'description' => $description,
+                //     'user_id' => auth()->user()->id,
+                //     'warehouse_id' => auth()->user()->role->warehouse_id
+                // ]);
+
+                $sale = new Transaction([
+                    'date_issued' => now(),
+                    'invoice' => $invoice,
+                    'product_id' => $item['id'],
+                    'quantity' => $request->transaction_type == 'Purchase' ? $item['quantity'] * -1 : $item['quantity'],
+                    'price' => $item['price'],
+                    'cost' => $cost,
+                    'transaction_type' => $request->transaction_type,
+                    'contact_id' => 1,
+                    'warehouse_id' => auth()->user()->role->warehouse_id,
+                    'user_id' => auth()->user()->id
+                ]);
+                $sale->save();
+
+                $sold = Product::find($item['id'])->sold + $item['quantity'];
+                Product::find($item['id'])->update(['sold' => $sold]);
+            }
+
+            DB::commit();
+
+            return response()->json([
+                'message' => 'Penjualan accesories berhasil, invoice: ' . $invoice,
+                'journal' => $journal
+            ], 201);
+        } catch (\Exception $e) {
+            DB::rollBack();
+            Log::error($e->getMessage());
+            return response()->json([
+                'success' => false,
+                'message' => 'Failed to create journal'
+            ], 500);
+        }
     }
 
     /**
