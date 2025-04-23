b588f72d98886a03299991034860a181ca3c6a13
fendnugraha <m@fends-MacBook-Air.local>
2025-04-23 11:47:47 +0700

    add log activity

--- app/Http/Controllers/LogActivityController.php
+++ app/Http/Controllers/LogActivityController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use App\Models\LogActivity;
 use Illuminate\Http\Request;
 use App\Http\Resources\AccountResource;
@@ -11,9 +12,12 @@ class LogActivityController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index($startDate, $endDate)
     {
-        $log = LogActivity::with(['user', 'warehouse'])->orderBy('created_at', 'desc')->paginate(5)->onEachSide(0);
+        $startDate = $startDate ? Carbon::parse($startDate)->startOfDay() : Carbon::now()->startOfDay();
+        $endDate = $endDate ? Carbon::parse($endDate)->endOfDay() : Carbon::now()->endOfDay();
+
+        $log = LogActivity::with(['user', 'warehouse'])->whereBetween('created_at', [$startDate, $endDate])->orderBy('created_at', 'desc')->paginate(5)->onEachSide(0);
 
         return new AccountResource($log, true, "Successfully fetched logs");
     }
