08593a638f830b107bd575c5ac63605744480056
fendnugraha <fend@fendnugrahas-MacBook-Air.local>
2025-07-26 14:18:36 +0700

    fix cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000'), 'https://team.threekomunika.com', 'https://team.three-komunika.com'],
+    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000'), 'https://team.three-komunika.com'],
 
     'allowed_origins_patterns' => [],
 
